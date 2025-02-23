401fb9bea25db85654acf5fa2d14b4aac591a3f7
TanyaSoni29 <tanu139sona@gmail.com>
2025-02-23 15:41:47 +0530

    Fake data fetching from json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 // import DateCounter from "./comaponent/DateCounter";
+import { useEffect } from "react";
 import Header from "./comaponent/Header";
+import { useReducer } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetch("http://localhost:8000/questions");
+        const questions = await data.json();
+        console.log(questions);
+        dispatch({ type: "dataReceived", payload: questions });
+      } catch (error) {
+        console.log(error);
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       {/* <DateCounter /> */}
